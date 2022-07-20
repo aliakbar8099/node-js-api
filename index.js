1b1a0112bf99646b8c42bc1e8274774ae4e802cd
aliakbar8099 <aliakbar.n8099@gamil.com>
2022-07-20 18:17:32 +0430

    sw

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const app = express();
 app.use(express.static('public'))
 
 // Middleware
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-
 app.use(cors({
     origin: '*'
 }));
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json());
 
 app.use("/admin", addProductRouter);
